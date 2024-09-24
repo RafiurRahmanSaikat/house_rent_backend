2e89f3ad3602f338bf76ba2317471a70f870464f
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2024-09-24 21:54:40 +0600

    Image Urls Fixed

--- rent_ease/settings.py
+++ rent_ease/settings.py
@@ -10,8 +10,6 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -161,6 +159,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
