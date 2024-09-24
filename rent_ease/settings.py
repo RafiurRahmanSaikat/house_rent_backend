495738c668b45bbd8c4d6f985b4c3b34c114972b
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2024-09-24 22:41:08 +0600

    DRF Settings Uupdated

--- rent_ease/settings.py
+++ rent_ease/settings.py
@@ -9,10 +9,8 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
@@ -20,8 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = True
-TIME_ZONE = "Asia/Dhaka"
-USE_TZ = True
+
 ALLOWED_HOSTS = [
     "*",
     "https://*.127.0.0.1",
