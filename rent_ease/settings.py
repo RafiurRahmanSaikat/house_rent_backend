9006ab0ffc9a55ea7d0138285ccffe1646e2d4d9
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2024-09-24 13:47:11 +0600

    Requirements Added

--- rent_ease/settings.py
+++ rent_ease/settings.py
@@ -97,17 +97,17 @@ WSGI_APPLICATION = "rent_ease.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 # DATABASES = {
-#     "default": dj_database_url.config(
-#         default=env("LIBRARY_DATABASE"),
-#     )
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+DATABASES = {
+    "default": dj_database_url.config(
+        default=env("LIBRARY_DATABASE"),
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
